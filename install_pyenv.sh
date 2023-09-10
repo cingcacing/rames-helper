2a193b298638ffe997ce7b9d5258b77acfc30499
Lars Ulrich <indra@kusuma.org>
2023-09-10 09:41:18 +0700

    add pyenv script

---
+++ install_pyenv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl https://pyenv.run | bash
+
+cat << EOF >> ~/.bashrc
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+EOF
+source ~/.bashrc
