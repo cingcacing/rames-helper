03122865507d6775c5ef2c057d5cd41fae858616
Lars Ulrich <indra@kusuma.org>
2023-11-28 15:17:37 +0700

    Update install_superset.sh

--- install_superset.sh
+++ install_superset.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 curl -sS https://raw.githubusercontent.com/apache/superset/2.0.0/requirements/base.txt | tail -n +10 | awk -v ORS=' ' '/^[A-z]/{print}' | xargs pip install apache-superset==2.0.0
-#curl -sS https://github.com/apache/superset/raw/3.0/requirements/base.txt | tail -n +10 | awk -v ORS=' ' '/^[A-z]/{print}' | xargs pip install apache-superset==3.0
+#curl -sS https://github.com/apache/superset/raw/3.0.2/requirements/base.txt | tail -n +10 | awk -v ORS=' ' '/^[A-z]/{print}' | xargs pip install apache-superset==3.0.2
