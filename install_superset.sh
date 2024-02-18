8d238af7f2f4c8d9e8930ad69113fbc89e92d831
Lars Ulrich <indra@kusuma.org>
2024-02-18 18:48:09 +0700

    Update install_superset.sh

--- install_superset.sh
+++ install_superset.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -sS https://raw.githubusercontent.com/apache/superset/2.0.0/requirements/base.txt | tail -n +10 | awk -v ORS=' ' '/^[A-z]/{print}' | xargs pip install apache-superset==2.0.0
-#curl -sS https://github.com/apache/superset/raw/3.0.2/requirements/base.txt | tail -n +10 | awk -v ORS=' ' '/^[A-z]/{print}' | xargs pip install apache-superset==3.0.2
+
+curl -sS https://raw.githubusercontent.com/cingcacing/rames-helper/main/resources/superset-mpack.txt | xargs pip3.9 install apache-superset==3.0.3
